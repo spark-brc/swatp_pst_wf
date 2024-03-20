e15c47d4caec14d202306aa23d1a34071e13bf88
Seonggyu Park <seonggyu.park@brc.tamus.edu>
2024-03-19 21:47:04 -0500

    added working dir arg

--- dependencies/swatp_pst/swatp_pst/analyzer.py
+++ dependencies/swatp_pst/swatp_pst/analyzer.py
@@ -305,9 +305,8 @@ def plot_onetone_ensembles(
     plt.show()
 
 
-
-
 def plot_prior_posterior_par_hist(
+        wd,
         pst, prior_df, post_df, sel_pars, 
         width=7, height=5, ncols=3, bestcand=None, parobj_file=None):
     nrows = math.ceil(len(sel_pars)/ncols)
@@ -1175,8 +1174,6 @@ def plot_sen_sobol2(wd, pst_file):
     ind = np.arange(N)  # the x locations for the groups
     width = 0.4
     phiwidth = 0.2
-    
-
     fig, ax = plt.subplots(figsize=(12,4))
     # Width of a bar 
     error_kw=dict(lw=1, capsize=2, capthick=1, alpha=0.5)
@@ -1360,7 +1357,7 @@ def jj_paper(wd1, wd2, pst_file1, pst_file2):
     sttsm1 = stts1.mean()
 
 
-def albufera_results():
+def albufera_predictive_results():
     wd = 'D:\\jj\\Albufera\\alb_nw_calibrated'
     colnam = 'et'
     pstfile = "alb_nw_ies.pst"
@@ -1382,8 +1379,127 @@ def albufera_results():
     plot_fill_between_ensembles(dff)
 
 
-    # print(pr_oe)
 
+def plot_prior_posterior_par_hist_jj(
+        width=4, height=3, ncols=3, bestcand=None, parobj_file=None, sharey=True):
+    
+    wd1 = 'D:\\jj\\opt_3rd\\optimized_results'
+    pstfile1 = "swatp_nw_ies.pst"
+    iter_idx1 = 5
+    pst1 = pyemu.Pst(os.path.join(wd1, pstfile1))
+    pst_nam1= pstfile1[:-4]
+    prior_df1 = pyemu.ParameterEnsemble.from_csv(
+        pst=pst1,filename=os.path.join(wd1,f"{pst_nam1}.{0}.par.csv"))
+    post_df1 = pyemu.ParameterEnsemble.from_csv(
+        pst=pst1,filename=os.path.join(wd1,f"{pst_nam1}.{iter_idx1}.par.csv"))
+    df_pars1 = pd.read_csv(os.path.join(wd1, f"{pst_nam1}.par_data.csv"))
+    sel_pars1 = df_pars1.loc[df_pars1["partrans"]=='log']
+    bstcs1="glm"
+    par_obj_file1 = f"{pst_nam1}.{iter_idx1}.par.objs.csv"
+    wd2 = 'D:\\jj\\Albufera\\alb_nw_calibrated'
+    pstfile2 = "alb_nw_ies.pst"
+    iter_idx2 = 4
+    pst2 = pyemu.Pst(os.path.join(wd2, pstfile2))
+
+    pst_nam2= pstfile2[:-4]
+    prior_df2 = pyemu.ParameterEnsemble.from_csv(
+        pst=pst2,filename=os.path.join(wd2,f"{pst_nam2}.{0}.par.csv"))
+    post_df2 = pyemu.ParameterEnsemble.from_csv(
+        pst=pst2,filename=os.path.join(wd2,f"{pst_nam2}.{3}.par.csv"))
+    df_pars2 = pd.read_csv(os.path.join(wd2, f"{pst_nam2}.par_data.csv"))
+    sel_pars2 = df_pars2.loc[df_pars2["partrans"]=='log']
+    bstcs2="glm"
+    par_obj_file2 = f"{pst_nam2}.{iter_idx2}.par.objs.csv"
+
+    pars_info1 = get_par_offset(pst1)
+    pars_info2 = get_par_offset(pst2)
+    colnams = [
+        "alpha", "awc", "cn2", "cn3_swf", "epco", 
+        "esco", "lat_len", "latq_co", "perco", "petco"
+    ]
+    for colnam in colnams:
+        fig, axes = plt.subplots(figsize=(width, height), nrows=1, ncols=2, sharey=True)
+        ax1 = fig.add_subplot(111, frameon=False)
+        ax1 = plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+        offset = pars_info1.loc[colnam, "offset"]
+        axes[0].hist(prior_df1.loc[:, colnam].values + offset,
+                bins=np.linspace(
+                    sel_pars1.loc[sel_pars1["parnme"]==colnam, 'parlbnd'].values[0]+ offset, 
+                    sel_pars1.loc[sel_pars1["parnme"]==colnam, 'parubnd'].values[0]+ offset, 20),
+                color = "gray", alpha=0.5, density=False,
+                label="Prior", orientation='horizontal'
+        )
+        y, x, _ = axes[0].hist(post_df1.loc[:, colnam].values + offset,
+                bins=np.linspace(
+                    sel_pars1.loc[sel_pars1["parnme"]==colnam, 'parlbnd'].values[0]+ offset, 
+                    sel_pars1.loc[sel_pars1["parnme"]==colnam, 'parubnd'].values[0]+ offset, 20), 
+                    alpha=0.5, density=False, label="Posterior", orientation='horizontal'
+        )
+        po_df1 = pd.read_csv(os.path.join(wd1, par_obj_file1))
+        x1 = po_df1.loc[po_df1["real_name"]=="46", colnam].values + offset
+        axes[0].axhline(y=x1, color='r', linestyle="--", alpha=0.5)
+        axes[0].invert_xaxis()
+        # axes[0].yaxis.set_label_position("right")
+        # axes[0].yaxis.tick_right()
+        # for tick in axes[0].yaxis.get_majorticklabels():
+        #     tick.set_horizontalalignment("left")
+
+        axes[0].set_title(colnam, fontsize=12, ha='left', x=0.07, y=0.97, backgroundcolor='white')
+
+        offset = pars_info2.loc[colnam, "offset"]
+        axes[1].hist(prior_df2.loc[:, colnam].values + offset,
+                bins=np.linspace(
+                    sel_pars2.loc[sel_pars2["parnme"]==colnam, 'parlbnd'].values[0]+ offset, 
+                    sel_pars2.loc[sel_pars2["parnme"]==colnam, 'parubnd'].values[0]+ offset, 20),
+                color = "gray", alpha=0.5, density=False,
+                label="Prior", orientation='horizontal'
+        )
+        y, x, _ = axes[1].hist(post_df2.loc[:, colnam].values + offset,
+                bins=np.linspace(
+                    sel_pars2.loc[sel_pars2["parnme"]==colnam, 'parlbnd'].values[0]+ offset, 
+                    sel_pars2.loc[sel_pars2["parnme"]==colnam, 'parubnd'].values[0]+ offset, 20), 
+                    alpha=0.5, density=False, label="Posterior", orientation='horizontal',
+                    color = "C1"
+        )
+        po_df2 = pd.read_csv(os.path.join(wd2, par_obj_file2))
+        x2 = po_df2.loc[po_df2["real_name"]=="glm", colnam].values + offset
+        axes[1].axhline(y=x2, color='r', linestyle="--", alpha=0.5)
+        for ax in axes:
+            ax.tick_params(axis='both', labelsize=12)
+            ax.yaxis.set_major_locator(FixedLocator([-100, 0, 100]))
+        # ax.tick_params(axis='x', labelsize=8)       
+        # plt.xlabel(r"Frequency", fontsize=10)
+        # plt.ylabel(r"Parameter relative change (%)", fontsize=10)
+        plt.tight_layout()
+        plt.subplots_adjust(wspace=0)
+        plt.savefig(f'{colnam}_hist.png', bbox_inches='tight', dpi=300)
+        plt.show()
+        print(os.getcwd())
+
+
+
+
+
+def albufera_par_results():
+    wd = 'D:\\jj\\Albufera\\alb_nw_calibrated'
+    pstfile = "alb_nw_ies.pst"
+    iter_idx = 4
+    # get_average_annual_wb(wd, colnam)
+  
+    pst = pyemu.Pst(os.path.join(wd, pstfile))
+    pst_nam = pstfile[:-4]
+    prior_df = pyemu.ParameterEnsemble.from_csv(
+        pst=pst,filename=os.path.join(wd,f"{pst_nam}.{0}.par.csv"))
+    post_df = pyemu.ParameterEnsemble.from_csv(
+        pst=pst,filename=os.path.join(wd,f"{pst_nam}.{3}.par.csv"))
+    df_pars = pd.read_csv(os.path.join(wd, f"{pst_nam}.par_data.csv"))
+    sel_pars = df_pars.loc[df_pars["partrans"]=='log']
+    bstcs="glm"
+    par_obj_file = f"{pst_nam}.{iter_idx}.par.objs.csv"
+    plot_prior_posterior_par_hist(wd,
+                                pst, prior_df, post_df, sel_pars,
+                                width=9, height=5, ncols=5,
+                                bestcand=bstcs, parobj_file=par_obj_file)
 
 
     # sfdf2 = read_sobol_sfi(wd2, pst_file2)
@@ -1427,7 +1543,7 @@ if __name__ == '__main__':
     
     
 
-    albufera_results()
+    plot_prior_posterior_par_hist_jj()
     
     
     
